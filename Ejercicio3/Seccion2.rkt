b1496726264e225578849c1776e0fe830804dec3
A01634310 <a01634310@itesm.mx>
2020-04-03 09:14:04 -0700

    Agregar problema 8, 10, 12 a Ejercicio 3

--- Ejercicio3/Seccion2.rkt
+++ Ejercicio3/Seccion2.rkt
@@ -38,3 +38,22 @@
         (if (null? (cdr lista1))
             #f
             (alguno? op (cdr lista1) (cdr lista2)))))
+
+; Problema 8 - verbosa
+;(define vinc (verbosa (lambda (x) (+ x 1))))
+(define (verbosa funcion)
+  (lambda (valor) (display (string-append "Entrada = " (number->string valor) "\n" "Salida = " (number->string (funcion valor))))))
+(define vinc (verbosa (lambda (x) (+ x 1))))
+
+; Problema 10 - impares
+;(impares '((1 2 3)(4 5 6)))
+(define (impares matrix)
+  (remove* '(()) (append-map (lambda (lista) (map (lambda (casilla) (if (odd? casilla) casilla null)) lista)) matrix)))
+
+; Problema 12 - multifnc
+;(multifnc '(sqr sqrt (lambda (v) (/ 1 v))) '(1 4 9))
+(define ns (variable-reference->namespace (#%variable-reference)))
+(define (string->procedure sym)
+  (eval sym ns))
+(define (multifnc operaciones valores)
+  (map (lambda (op) (map (string->procedure op) valores)) operaciones))
