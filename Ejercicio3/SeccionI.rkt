8169a2040ee6ca8ac3fb060338f898b354669cc1
A01634310 <a01634310@itesm.mx>
2020-04-07 01:29:04 -0600

    Primeros dos problemas Tarea 4

--- Ejercicio3/SeccionI.rkt
+++ Ejercicio3/SeccionI.rkt
@@ -1,4 +1,5 @@
 #lang racket
+; Problema 1
 ; (unos '(0 0 0 0 1 1))
 (define (unos input)
   (unos-aux input 0 (length input)))
@@ -7,12 +8,14 @@
         [(= (car lista) 1) (- length pos)]
         [else (unos-aux (cdr lista) (+ pos 1) length)]))
 
+; Problema 2
 ; (invierte '((0 1 0)(1 0 1)))
 (define (invierte matrix)
   (cond [(empty? matrix) null]
         [(list? (car matrix)) (cons (invierte (car matrix)) (invierte (cdr matrix)))]
         [else (if (= (car matrix) 0) (cons 1 (invierte (cdr matrix))) (cons 0 (invierte (cdr matrix))))]))
 
+; Problema 3
 ; (parcial 2 '((a01111111 (Jorge Perez) (100 100)) (a02222222 (Gloria Flores) (90)) (a03333333 (Ramiro Mendez) (90 60 90))))
 (define (parcial index matrix)
   (cond [(empty? matrix) null]
@@ -21,6 +24,7 @@
 (define (parcial-aux index califs)
   (if (> index (length (car califs))) (cons 'NO '()) (cons (list-ref (car califs) (- index 1)) '())))
 
+; Problema 4
 ; (subarbol 'b '(a(b(c()())(d()()))(e()(f(g()())()))))
 (define (subarbol id arbol)
   (cond [(empty? arbol) null]
@@ -30,6 +34,7 @@
         [(not (null? (subarbol id (caddr arbol)))) (subarbol id (caddr arbol))]
         [else null]))
 
+; Problema 5
 ; (adyacentes 3 '((1 2 3 4)((1 2)(1 4)(2 3)(2 4)(3 4))))
 ; (adyacentes 'a '((a b c)((a b)(c b)(c a))))
 (define (adyacentes target lista)
---
+++ Tarea4/A00_A01634310_Tarea4.rkt
@@ -0,0 +1,118 @@
+#lang racket
+; A01634310 | Diego Estrada Talamantes
+; A00       | Alberto Garcia Viegas
+
+
+
+; Problema 1 - Árboles Binarios ------------------------------------------------------------------
+
+; Arbol para pruebas
+(define AB '(8 (5 (2 () ())
+                  (7 () ()))
+               (9 ()
+                  (15 (11 () ())
+                      () ))))
+
+; 1.a) Define si el formato es congruente a arboles binarios
+; (arbol-binario? '()) => #t
+; (arbol-binario? AB) => #t
+; (arbol-binario? '(a (b (() ()) ()) (d () ()))) => #f
+(define (arbol-binario? arbol)
+  (cond [(empty? arbol) true]
+        [(= (length arbol) 3) (and (arbol-binario? (cadr arbol)) (arbol-binario? (caddr arbol)))]
+        [else false]))
+
+; 1.b) Regresa los numeros menores al indicado en un arbol
+; (obten-menores AB 2) => ()
+; (obten-menores AB 8) => (5 3 7)
+; (obten-menores AB 20) => (8 5 2 7 9 15 11)
+(define (obten-menores arbol target)
+  (cond [(empty? arbol) '()]
+        [(< (car arbol) target) (flatten (list (car arbol) (obten-menores (cadr arbol) target) (obten-menores (caddr arbol) target)))]
+        [else (obten-menores (cadr arbol) target)]))
+
+; 1.c) Regresa los valores de la rama más larga de un arbol
+; (rama+larga '()) => ()
+; (rama+larga AB) => (8 9 15 11)
+; (rama+larga '(a (b (c () ()) ()) (d (e (f (g (h () ()) ()) ()) ()) ()))) => (a b c)
+(define (rama+larga arbol)
+  (cond [(empty? arbol) '()]
+        [(>= (length (rama+larga (cadr arbol))) (length (rama+larga (caddr arbol)))) (cons (car arbol) (rama+larga (cadr arbol)))]
+        [else (cons (car arbol) (rama+larga (caddr arbol)))]))
+
+
+
+; Problema 2 - Grafo Dirigido ------------------------------------------------------------------
+
+; Grafo para pruebas
+(define g
+  '((A ((B 2) (D 10)))
+    (B ((C 9) (E 5)))
+    (C ((A 12) (D 6)))
+    (D ((E 7)))
+    (E ((C 3)))
+    ))
+
+; 2.1) Regresa los nodos dostino de un nodo en particular del grafo
+; (nodos-destino g 'A) => (B D)
+; (nodos-destino g 'D) => (E)
+; (nodos-destino g 'F) => ()
+(define (nodos-destino grafo target)
+  (cond [(empty? grafo) '()]
+        [else
+         (if (equal? (caar grafo) target)
+             (flatten (nodos-destino-aux (cadar grafo)))
+             (nodos-destino (cdr grafo) target))
+         ]))
+(define (nodos-destino-aux conexiones)
+  (cond [(empty? conexiones) '()]
+        [else (flatten (list (caar conexiones) (nodos-destino-aux (cdr conexiones))))]))
+
+; 2.2) Regresa los nodos origen que tiene un nodo en particular del grafo
+; (nodos-origen g 'A) => (C)
+; (nodos-origen g 'C) => (B E)
+; (nodos-origen g 'F) => ()
+(define (nodos-origen grafo target)
+  (cond [(empty? grafo) '()]
+        [(list? (car grafo)) (flatten (list (nodos-origen-aux (car grafo) target) (nodos-origen (cdr grafo) target)))]
+        [else '()]))
+(define (nodos-origen-aux nodo target)
+  (cond [(member target (flatten (cdr nodo))) (car nodo)]
+        [else '()]))
+
+; 2.3) Eliminar un arco en especifico del grafo otorgado
+; (elimina-arco g 'B 'C) => ((A ((B 2) (D 10))) (B ((E 5))) (C ((A 12) (D 6))) (D ((E 7)))(E ((C 3)))))
+; (elimina-arco g 'B 'D) => ((A ((B 2) (D 10))) (B ((C 9) (E 5))) (C ((A 12) (D 6))) (D ((E 7)))(E ((C 3)))))
+; (elimina-arco g 'F 'C) => ((A ((B 2) (D 10))) (B ((C 9) (E 5))) (C ((A 12) (D 6))) (D ((E 7)))(E ((C 3)))))
+(define (elimina-arco grafo orig dest)
+  (cond [(empty? grafo) '()]
+        [else
+         (if (equal? (caar grafo) orig)
+             (cons (elimina-arco-aux (car grafo) dest) (elimina-arco (cdr grafo) orig dest))
+             (cons (car grafo) (elimina-arco (cdr grafo) orig dest)))
+         ]))
+(define (elimina-arco-aux nodo dest)
+  (cons (car nodo) (list (elimina-arco-aux-aux (cadr nodo) dest))))
+(define (elimina-arco-aux-aux conexiones dest)
+  (cond [(empty? conexiones) '()]
+        [(equal? (caar conexiones) dest) (cdr conexiones)]
+        [else (cons (car conexiones) (elimina-arco-aux-aux (cdr conexiones) dest))]))
+
+
+
+; Problema 3 - Funciones de orden superior ------------------------------------------------------------------
+
+; 3.1) Contar la cantidad de ceros dentro de una matriz
+; (cuenta-ceros '()) => 0
+; (cuenta-ceros '((0 1)(2 3))) => 1
+; (cuenta-ceros '((4 0 3 1)(5 1 2 1)(6 0 1 1))) => 2
+
+
+; 3.2) Regresar lista con el valor menor y mayor dentro de una matriz
+; (minmax '((2))) => (2 2)
+; (minmax '((0 1)(2 3))) => (0 3)
+; (minmax '((4 0 -3 1)(5 -1 2 1)(6 0 1 1))) => (-3 6)
+
+
+; 3.3) Multiplicación de matrices
+; (multmat '((1 2 3)(0 2 1)) '((4 0 3 1)(5 1 2 1)(6 0 1 1))) => ((32 2 10 6)(16 2 5 3))
